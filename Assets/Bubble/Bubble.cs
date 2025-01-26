54fce9a84cbe74a33e444eca5f30913423a753a2
apmoon <85451021+AbdullahKarakuss@users.noreply.github.com>
2025-01-26 14:35:01 +0300

    Revert "Merge pull request #6 from Gloriens/Scenes"
    This reverts commit 4dc0338e590ac1f078d46c4a0d62cbab173e2065, reversing
    changes made to e75cd6a05725c1d9bc5f35aab41aba586d9be34c.

--- Assets/Bubble/Bubble.cs
+++ Assets/Bubble/Bubble.cs
@@ -1,70 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
-    [SerializeField] private int health = 10; 
-    private int counter = 0;
-    private Animator animator; 
+    [SerializeField] private int health;
+    private int counter;
+   
     
-    
+    // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>(); 
+       
     }
 
-
+    // Update is called once per frame
     void Update()
     {
-        UpdateAnimationState();
+        
     }
+    
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        
         if (other.gameObject.CompareTag(gameObject.tag))
         {
             Debug.Log("We are the same element!");
             counter++;
-            
-            if (counter == 10) 
+            if (counter == 5)
             {
-                animator.SetTrigger("Happy");
-                Debug.Log("You won the game!");
+                Debug.Log("You won the game");
             }
+            
+        }else if (other.CompareTag("Player")!)
+        {
+            Debug.Log("girdi");
         }
     }
 
-    public void TakeDamage()
+    public void takeDamage()
     {
         health--;
-        Debug.Log("Health: " + health);
-
-        if (health <= 0)
+        Debug.Log(health);
+        if (health == 0)
         {
-            Debug.Log("Bubble destroyed!");
-            animator.SetTrigger("Death");
-            Destroy(gameObject, 1f); 
-        }
-    }
-
-    private void UpdateAnimationState()
-    {
-        if (health <= 2)
-        {
-            animator.SetTrigger("Death");
-        }
-        else if (health <= 4)
-        {
-            animator.SetTrigger("Cry");
-        }
-        else if (health <= 6)
-        {
-            animator.SetTrigger("Sad");
-        }
-        else if (health <= 8)
-        {
-            animator.SetTrigger("Idle");
+            Destroy(gameObject);
         }
     }
 }
